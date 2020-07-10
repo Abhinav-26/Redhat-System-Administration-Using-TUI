f4564aa882add58e29cbf533cbd31e4a319cfcf1
Abhinav-26 <abhinavdubey616@gmail.com>
2020-07-11 00:12:07 +0530

    Logs Configuration

---
+++ LogConfig.py
@@ -0,0 +1,20 @@
+import os
+
+def LogsManagement():
+	
+	os.system("clear")
+	os.system("tput setaf 2")
+	print("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	os.system("tput setaf 3")
+	print("\t\t\tPACKAGE MANAGEMENT USING TUI")
+	os.system("Logsput setaf 2")
+	print("\t\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	os.system("tput setaf 6")
+	print("""Select the number to perform the task :\n
+Press 01: TO SEE ALL LOGS FILE PRESENT	
+Press 02: TO OPEN A LOG FILE
+Press 03: TO CONTINOUS MONITOR OF ANY LOG FILE
+Press 04: TO EXTRACT WEBSERVER DATA FOR ANALYSIS
+Press 05: 
+Press 06: TO EXIT\n""")
+
